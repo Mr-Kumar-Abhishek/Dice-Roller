5f2f6f472a31d283c11065aab37fc7d5b3edc117
Abhishek Kumar <mr.kumar.abhishek@email.com>
2022-06-17 18:29:47 +0530

    set demo code for dice roll with images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -26,9 +26,8 @@ class MainActivity : AppCompatActivity() {
         val diceRoll = dice.roll()
 
         // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-
+        val diceImage: ImageView = findViewById(R.id.imageView2)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 }
 class Dice(private val numSides: Int) {
